0ddc1550a1460d9f77c49f0f6fe28f9fcac2527e
priyaachauhan2000-alt <priya.a.chauhan2000@gmail.com>
2026-06-02 10:42:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,26 @@
 import streamlit as st
 import pandas as pd
 
-st.title("Phase 2 File Inspection")
+st.set_page_config(layout="wide")
+
+st.title("Britannia Phase 2 Inspection")
 
 file_path = "Britannia_Financial_Intelligence_Phase2.xlsx"
 
 xls = pd.ExcelFile(file_path)
 
-st.write("Sheets:")
 st.write(xls.sheet_names)
 
 for sheet in xls.sheet_names:
-    st.subheader(sheet)
+
+    st.divider()
+
+    st.header(sheet)
 
     df = pd.read_excel(file_path, sheet_name=sheet)
 
-    st.write(df.head())
+    st.write("Columns:")
+
+    st.write(list(df.columns))
+
+    st.dataframe(df.head(10))
