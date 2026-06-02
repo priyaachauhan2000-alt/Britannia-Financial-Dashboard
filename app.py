a8e8c25f942d24107d85120b9764b829d99c6799
priyaachauhan2000-alt <priya.a.chauhan2000@gmail.com>
2026-06-02 11:13:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+import plotly.graph_objects as go
+
 monthly = pd.read_excel(
     "Britannia_Financial_Intelligence_Phase2.xlsx",
     sheet_name="Monthly_Report"
 )
 
-# Remove the last two bad rows
 monthly = monthly.iloc[:-2]
 
-# Reset index
 monthly = monthly.reset_index(drop=True)
 
 latest = monthly.iloc[-1]
